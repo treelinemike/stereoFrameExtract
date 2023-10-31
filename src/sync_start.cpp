898422e5cade47e46c2496cec17093245d435e3c
Mike Kokko <mike.kokko@gmail.com>
2023-10-31 12:28:41 -0400

    got sync_start using cxxopts and yaml-cpp

--- src/sync_start.cpp
+++ src/sync_start.cpp
@@ -12,6 +12,8 @@
 #include <netinet/in.h> 
 #include <string.h>
 #include <iostream> 
+#include <cxxopts.hpp>  // https://www.github.com/jarro2783/cxxopts
+#include <yaml-cpp/yaml.h>
 
 #define USE_HYPERDECK_LEFT true
 #define USE_HYPERDECK_RIGHT true
@@ -29,7 +31,7 @@ class SyncDevice{
 private:
 	int socket_fd, status, res, opt, valread;
 	struct sockaddr_in address; 
-	std::string ip_address;
+	std::string ip_address, dev_name;
 	syncDeviceType type;
 	bool readyToConnect = false;
 	bool socketInitialized = false;
@@ -39,12 +41,12 @@ private:
 	struct timeval timeout;
 	char recordCommand[10] = "ping\n";
 public:
-	SyncDevice(std::string arg_ip, int arg_port, syncDeviceType arg_type){
+	SyncDevice(std::string arg_dev_name, std::string arg_ip, int arg_port, syncDeviceType arg_type){
+		dev_name = arg_dev_name;
 		ip_address = arg_ip;
 		port = arg_port;
 		type = arg_type;
 		readyToConnect = true;
-		Init();
 	}
 	~SyncDevice(){
 		close(socket_fd);
@@ -187,7 +189,112 @@ public:
 // main
 int main(int argc, char const *argv[]) 
 { 
+
+	// variables
+	cxxopts::Options options("sync_start", "LAN-based device synchronization");
+	YAML::Node config;
+	std::string yamlfile_name, device_name, device_type_str, device_ip;
+	unsigned int device_port;
+	bool manual_start_flag, device_active;
+	std::vector<SyncDevice> devices;
+	syncDeviceType device_type;
+
+	// PARSE COMMAND LINE OPTIONS
+	try
+	{
+		options.add_options()
+		("y,yamlconfig", "name of config YAML file (REQUIRED)", cxxopts::value<std::string>());
+		auto cxxopts_result = options.parse(argc, argv);
+		if (cxxopts_result.count("yamlconfig") != 1)
+		{
+			std::cout << options.help() << std::endl;
+			return -1;
+		}
+		yamlfile_name = cxxopts_result["yamlconfig"].as<std::string>();
+	}
+	catch (const cxxopts::exceptions::exception& e)
+	{
+		std::cout << "Error parsing options: " << e.what() << std::endl;
+		return -1;
+	}
 	
+	// LOAD CONFIGURATION FILE
+	try {
+		// get input file
+		config = YAML::LoadFile(yamlfile_name);
+
+		// get manual start flag
+		if (config["manual_start"]) {
+			manual_start_flag = config["manual_start"].as<bool>();
+		}
+		else {
+			manual_start_flag = true;
+			std::cout << "YAML: no 'manual_start' key in file, assuming manual start" << std::endl;
+		}
+
+		// parse each clip definition
+		if (!config["devices"]) {
+			std::cout << "ERROR: no 'devices' key found in YAML config file" << std::endl;
+			return -1;
+		}
+		YAML::Node yamldevices = config["devices"];
+		for (YAML::const_iterator it = yamldevices.begin(); it != yamldevices.end(); ++it) {
+			device_name = it->first.as<std::string>();
+			
+			// get the YAML node for this device description
+			YAML::Node device_details = it->second;
+			
+			// skip this device if we're not intersted in synchronizing it (i.e. if active == false)
+			device_active = device_details["active"].as<bool>();
+			if(!device_active)
+				continue;
+			
+			// extract device info
+			// TODO: add error checking
+			device_type_str = device_details["type"].as<std::string>();
+			device_ip = device_details["ip"].as<std::string>();
+			device_port = device_details["port"].as<unsigned int>();
+			
+			// map device_type_str from std::string to enum
+			// TODO: is there a better way to bind enum?
+			if(device_type_str.compare("HyperDeck")){
+				device_type = HyperDeck;
+			} else if(device_type_str.compare("Kinematics")){
+				device_type = Kinematics;
+			} else if(device_type_str.compare("Other")){
+				device_type = Other;
+			} else {
+				std::cout << "Invalid device type: " << device_type_str << std::endl;
+				return -1;
+			}
+			
+			// create a device
+			// constructor: SyncDevice(std::string arg_dev_name, std::string arg_ip, int arg_port, syncDeviceType arg_type)
+			std::cout << "Ading " << device_ip << ":" << device_port << " as " << device_name << std::endl;
+			SyncDevice single_device(device_name,device_ip,device_port,device_type); // on stack (not heap), will be automatically freed 
+			devices.push_back(single_device); // object is *copied* into vector
+		}
+
+		// make sure we have some clip definitions in our vector
+		if (!devices.size()) {
+			std::cout << "ERROR: no valid clip definitions found in YAML config file" << std::endl;
+			return -1;
+		}
+	}
+	catch (const YAML::Exception& e)
+	{
+		std::cout << "ERROR PARSING YAML CONFIGURATION: " << e.what() << std::endl;
+		return -1;
+	}
+
+	
+	
+	if(manual_start_flag){
+		std::cout << "Press ENTER to synchronize devices" << std::endl;
+        std::cin.get();
+    }
+	
+	/*
 	SyncDevice* hyperdeck_left = new SyncDevice("192.168.10.50",9993,HyperDeck);
 	hyperdeck_left->Configure();
 	
@@ -202,6 +309,9 @@ int main(int argc, char const *argv[])
 	hyperdeck_left->Close();
 	hyperdeck_right->Close();
 	kin_pc->Close();
+	*/
 	
+	// done
+	std::cout << "done" << std::endl;
 	return 0; 
 }
